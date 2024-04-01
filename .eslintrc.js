9f7aa7c25f8e214ee7f415996be38e0899cd6ea0
Damon Smith <cleverdamontoutube@gmail.com>
2024-04-01 13:18:54 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,29 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
     'plugin:prettier/recommended',
-    'prettier/react',
   ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2024,
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': 'error',
+
+    // Turn off unnecessary TypeScript rules
+    '@typescript-eslint/explicit-function-return-type': 'off', // Allow omitting return types on functions
+    '@typescript-eslint/interface-name-prefix': 'off', // Allow interface names to start with "I"
+    '@typescript-eslint/no-var-requires': 'off', // Allow using require for imports
+    '@typescript-eslint/no-namespace': 'off', // Allow using namespaces
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Allow implicit return types for exported functions
   },
 };
